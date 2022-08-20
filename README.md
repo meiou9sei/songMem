030bc6b81ec38573c650315a456ff55f1c03e373
Cameron Avvampato <87138078+meiou9sei@users.noreply.github.com>
2022-08-20 11:36:33 -0400

    plan out data to track and process of app

--- README.md
+++ README.md
@@ -2,12 +2,12 @@
 
 ## An app to help you memorize song lyrics
 
-- Paste your song lyrics into a form, lines separated by a newline. The algorithm automatically generates cards for you for each sentence in the song (that you can edit).
-- You can then group these sentences into sections and the app will assign an emoji to them (which you can change to another emoji, or custom string).
+- Paste your song lyrics into a form, lines separated by a newline. The algorithm automatically generates lines for you for each sentence in the song (that you can edit).
+- You then group these sentences into sections and the app will assign an emoji to them (which you can change to another emoji, or custom string).
 - You can then start memorizing the song. You can display the whole line, just the first 2 words, a custom string, or hide the line entirely.
 - You can change the display of whole sections of line at once. 
 - In sing mode, you use the arrow keys/mouse click to go through a card at a time of sections. Sing along while looking at the cards.
-- You can mark each section as "mem'd!" or "not mem'd", to know which sections to review. 
+- You can mark each section as "memorized!" or "not memorized", to know which sections to review. 
 - store song lyrics you created in local storage / can export them as json files?
 
 - light and dark modes w/ styled components
--- src/App.js
+++ src/App.js
@@ -7,3 +7,60 @@ function App() {
 }
 
 export default App;
+
+/* 
+data we need to track: 
+  -- songMem
+    -- id (probably just the title of the song)
+    -- lines array
+    -- sections array
+  -- lines
+    -- id
+    -- the string of lyric itself
+    -- has a section it's assigned to
+    -- a label (be it the first two words or a custom strong)
+    -- display status (all, part, hidden)
+  -- sections
+    -- id
+    -- a label (be it an emoji or a custom string)
+    -- which line ids it contains
+    -- memorized status (true or false)
+    -- display status (none, all, part, hidden)
+
+process: 
+  -- view saved songMems, enter songMem, or enter new lyrics
+  new lyrics:
+    -- enter title (checks if title already exists)
+    -- enter lyrics to field, lines separated by a newline, and submit*
+      -- songMem object with array of line objects generated
+    -- user can group them into sections
+      -- sections added to songMem object as an array
+      -- emoji generated for section
+    
+  enter songMem string:
+  -- checks if title already exists
+  -- parses through string, stores in local storage, displays on screen
+
+  edit songMem lyric:
+    -- change title
+      -- checks if title already exists
+    -- change display setting of line, or groups of lines by section
+      -- first 2 words, custom string, or hide line entirely
+    -- change custom label for line 
+    -- change custom label for section
+    -- change memorized status of sections
+
+  sing mode:
+    -- cards appear in sequence, one card on main display, can navigate through them w/ arrow keys or mouse clicks
+      -- can change their memorized status too
+    
+  saving:
+    -- status change of cards/lyrics is automatically stored 
+    -- can delete songMems
+    -- can load songMem
+
+
+  * I don't think users should be able to edit lyrics?
+    - lyrics don't change, and allowing them to edit it will cause a lot of hassle
+    - need to restart by pasting new lyrics for now, maybe add edit option later 
+*/
