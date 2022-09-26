ba53b510d50600a192da2f28646321ae7149155a
Cameron Avvampato <87138078+meiou9sei@users.noreply.github.com>
2022-09-26 18:15:04 -0400

    new songs submitted are formatted w/ title, writer, and lyrics formatted to object properly

--- src/components/NewSongInput.js
+++ src/components/NewSongInput.js
@@ -5,33 +5,87 @@ import { StyledNewSongInput } from "./styles/NewSongInput.styled";
 
 const NewSongInput = ({childToParent}) => {
     const [songTitle, setSongTitle] = useState('');
+    const [songWriter, setSongWriter] = useState('');
     const [songLyrics, setSongLyrics] = useState('');
     const [songMem, setSongMem] = useState('songMemPrioString');
     const handleSubmit = event => {
-        console.log('test handlesubmit ran');
         event.preventDefault();
 
         //access input values
         console.log('song Title is', songTitle);
+        console.log('song Writer is', songWriter);
         console.log('song Lyrics are', songLyrics);
 
-        formatSong(songTitle, songLyrics);
+        formatSong(songTitle, songWriter, songLyrics);
 
         //clear input values
         setSongTitle('');
+        setSongWriter('');
         setSongLyrics('');
     }
-    const formatSong = (songTitle, songLyrics) => {
-        setSongMem('The song title is ' + songTitle + ' and the lyrics are ' + songLyrics + '.'); //bunch of code here to format it
+
+    const formatSong = (songTitle, songWriter, songLyrics) => {
+        setSongMem('The song title is ' + songTitle + ' by ' + songWriter + ' and the lyrics are ' + songLyrics + '.'); //bunch of code here to format it
+        
+        let numberOfLineBreaks = (songLyrics.match(/\n/g)).length;
+        
+        console.log('number of line breaks is ' + numberOfLineBreaks)
+
+        const tempSongMemObject = {
+            id: songTitle + ' by ' + songWriter,
+            lines: [],
+            sections: null,
+        }
+
+        const songLyricsArray = songLyrics.split(/\n/g)
+
+        //add lines to object
+        for (let i = 1; i < numberOfLineBreaks + 1; i++) {
+            tempSongMemObject.lines.push({
+                id: numberOfLineBreaks,
+                line: songLyricsArray.shift(),
+                section: null,
+                hint: null,
+                displayStatus: null
+            });
+        }
+        console.log(tempSongMemObject);
     }
+
     return (
         <StyledNewSongInput onSubmit={handleSubmit} >
             <label htmlFor="songTitle">Title:</label>
-            <input type="text" name="songTitle" id="songTitle" onChange={event => setSongTitle(event.target.value)} value={songTitle} />
+            <input
+                required
+                type="text" 
+                name="songTitle" 
+                id="songTitle" 
+                onChange={e => setSongTitle(e.target.value)}
+                value={songTitle} 
+            />
+            <label htmlFor="songWriter">Songwriter:</label>
+            <input
+                required
+                type="text" 
+                name="songWriter" 
+                id="songWriter" 
+                onChange={e => setSongWriter(e.target.value)}
+                value={songWriter} 
+            />
             <label htmlFor="songLyrics">Lyrics:</label>
-            <textarea name="songLyrics" id="songLyrics" placeholder="Enter your Song lyrics here. Separate each line by line." onChange={event => setSongLyrics(event.target.value)} value={songLyrics} />
-            <input type="submit" value="Generate New SongMem" />
-            <button onClick={() => childToParent(songMem)}>send songmem up</button>
+            <textarea
+                required
+                name="songLyrics" 
+                id="songLyrics" 
+                placeholder="Enter your Song lyrics here. Separate each line by line." 
+                onChange={e => setSongLyrics(e.target.value)}
+                value={songLyrics}
+            />
+            <input 
+                type="submit" 
+                value="Generate New SongMem" 
+                onClick={() => childToParent(songMem)}
+            />
         </StyledNewSongInput>
     );
 }
